6ca6d09938453c187ce6cf77e2266a82cafb0231
miaepark <44250577+miaepark@users.noreply.github.com>
2018-12-10 13:31:25 -0800

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+##reading the data general and training
+
+featureNames        <- read.table("./features.txt",header=FALSE)
+activityLabel   <- read.table("./activity_labels.txt",header=FALSE)
+subject_Train    <-read.table("./train/subject_train.txt", header=FALSE)
+X_train          <- read.table("./train/X_train.txt", header=FALSE)
+y_train          <- read.table("./train/y_train.txt", header=FALSE)
+
+names(subject_train) <- "subjectID"
+names(subject_test) <- "subjectID"
+
+# add column names for measurement files
+names(X_train) <- featureNames$V2
+names(X_test) <- featureNames$V2
+
+# add column name for label files
+names(y_train) <- "activity"
+names(y_test) <- "activity"
+
+# combine files into one dataset
+train <- cbind(subject_train, y_train, X_train)
+test <- cbind(subject_test, y_test, X_test)
+combined <- rbind(train, test)
+
+
+## STEP 2: Extracts only the measurements on the mean and standard
+## deviation for each measurement.
+
+# determine which columns contain "mean()" or "std()"
+meanstdcols <- grepl("mean\\(\\)", names(combined)) |
+    grepl("std\\(\\)", names(combined))
+
+# ensure that we also keep the subjectID and activity columns
+meanstdcols[1:2] <- TRUE
+
+# remove unnecessary columns
+combined <- combined[, meanstdcols]
+
+
+## STEP 3: Uses descriptive activity names to name the activities
+## in the data set.
+## STEP 4: Appropriately labels the data set with descriptive
+## activity names. 
+
+# convert the activity column from integer to factor
+combined$activity <- factor(combined$activity, labels=c("Walking",
+    "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
+
+
+## STEP 5: Creates a second, independent tidy data set with the
+## average of each variable for each activity and each subject.
+
+# create the tidy data set
+melted <- melt(combined, id=c("subjectID","activity"))
+tidy <- dcast(melted, subjectID+activity ~ variable, mean)
+
+# write the tidy data set to a file
+write.csv(tidy, "tidy.csv", row.names=FALSE)
